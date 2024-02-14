3bde3ffc9129b039b1ba6d5b11ff2c73d450bfdb
Jakub Furtak <kuba.furtak.pl@gmail.com>
2024-02-14 17:24:18 +0100

    modified the CommentsView.vue

---
+++ Frontend/src/components/comments/CommentsMainContent.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="flex">
+        <div class="bg-[#1a1a1b] rounded-l-md w-10 text-white">
+            <div class="px-2 text-center flex flex-col items-center gap-1 mt-2">
+                <div class="w-5">
+                    <ArrowUpIcon/>
+                </div>
+
+                <p class="text-white">3.3k</p>
+
+                <div class="w-5">
+                    <ArrowDownIcon/>
+                </div>
+
+            </div>
+        </div>
+        <div class="bg-[#1a1a1b] rounded-r-md grow flex flex-col *:mx-2">
+            <div class=" text-sm mt-2 flex gap-1">
+                <p class="text-white">r/shitposting</p>
+                <p class="text-gray-400">Posted by u/Comfortable_Rush2528 5 hours ago</p>
+            </div>
+            <div class="text-white text-xl">Mam dosyć</div>
+            <div class="">
+                <p class=" text-white">
+                    Mam 26 lat. Magistra na karku. Do wczoraj marzenia. Tyle lat nauki, podczas studiów
+                    dodatkowe kursy.
+                    Cały czas się rozwijam i douczam. Na samym początku kariery trafiłam na bardzo toksyczne
+                    miejsce gdzie
+                    każdy starał się zgnoić młodego a druga połowa udawała że nie istnieje. Rzuciłam tą
+                    pracę bo
+                    nie dawałam
+                    rady psychicznie, zaczęły się napady paniki, sraczki i rzyganie na samą myśl o pracy.
+                    Potem
+                    starałam się
+                    dostać pracę wszędzie. Nie tylko w zawodzie. Na biedronki i sklepy mam za wysokie
+                    wykształcenie i to
+                    jeszcze nie w takim kierunku co by chcieli. Na jakieś pomoce nauczyciela w przedszkolach
+                    również (mimo
+                    że mam tu doświadczenie) cytat "bo gdzie pani ze swoimi studiami będzie pupy podcierać
+                    dzieciom".
+                    Składałam CV do prac wszędzie i chyba do każdej firmy w moim mieście. Jedynie gdzie
+                    wybrzydzam to praca
+                    fizyczna (również ze względów złych pleców) no i seks praca. Jestem wykształconą osobą.
+                    Poszerzającą
+                    swoją wiedzę a kurwa od 4 miesięcy nie mogę znaleźć pracy. Przerasta mnie to. Nie wiem
+                    co
+                    mam robić.
+                    Chodzenie do firm. Pisanie maili i dzwonienie nawet jak nie szukają nie pomaga. Od kilku
+                    miesięcy
+                    wszystko co mogło pójść źle idzie źle. Nie wiem co mam dalej robićMam 26 lat. Magistra
+                    na karku. Do wczoraj marzenia. Tyle lat nauki, podczas studiów
+                    dodatkowe kursy.
+                    Cały czas się rozwijam i douczam. Na samym początku kariery trafiłam na bardzo toksyczne
+                    miejsce gdzie
+                    każdy starał się zgnoić młodego a druga połowa udawała że nie istnieje. Rzuciłam tą
+                    pracę bo
+                    nie dawałam
+                    rady psychicznie, zaczęły się napady paniki, sraczki i rzyganie na samą myśl o pracy.
+                    Potem
+                    starałam się
+                    dostać pracę wszędzie. Nie tylko w zawodzie. Na biedronki i sklepy mam za wysokie
+                    wykształcenie i to
+                    jeszcze nie w takim kierunku co by chcieli. Na jakieś pomoce nauczyciela w przedszkolach
+                    również (mimo
+                    że mam tu doświadczenie) cytat "bo gdzie pani ze swoimi studiami będzie pupy podcierać
+                    dzieciom".
+                    Składałam CV do prac wszędzie i chyba do każdej firmy w moim mieście. Jedynie gdzie
+                    wybrzydzam to praca
+                    fizyczna (również ze względów złych pleców) no i seks praca. Jestem wykształconą osobą.
+                    Poszerzającą
+                    swoją wiedzę a kurwa od 4 miesięcy nie mogę znaleźć pracy. Przerasta mnie to. Nie wiem
+                    co
+                    mam robić.
+                    Chodzenie do firm. Pisanie maili i dzwonienie nawet jak nie szukają nie pomaga. Od kilku
+                    miesięcy
+                    wszystko co mogło pójść źle idzie źle. Nie wiem co mam dalej robićMam 26 lat. Magistra
+                    na karku. Do wczoraj marzenia. Tyle lat nauki, podczas studiów
+                    dodatkowe kursy.
+                    Cały czas się rozwijam i douczam. Na samym początku kariery trafiłam na bardzo toksyczne
+                    miejsce gdzie
+                    każdy starał się zgnoić młodego a druga połowa udawała że nie istnieje. Rzuciłam tą
+                    pracę bo
+                    nie dawałam
+                    rady psychicznie, zaczęły się napady paniki, sraczki i rzyganie na samą myśl o pracy.
+                    Potem
+                    starałam się
+                    dostać pracę wszędzie. Nie tylko w zawodzie. Na biedronki i sklepy mam za wysokie
+                    wykształcenie i to
+                    jeszcze nie w takim kierunku co by chcieli. Na jakieś pomoce nauczyciela w przedszkolach
+                    również (mimo
+                    że mam tu doświadczenie) cytat "bo gdzie pani ze swoimi studiami będzie pupy podcierać
+                    dzieciom".
+                    Składałam CV do prac wszędzie i chyba do każdej firmy w moim mieście. Jedynie gdzie
+                    wybrzydzam to praca
+                    fizyczna (również ze względów złych pleców) no i seks praca. Jestem wykształconą osobą.
+                    Poszerzającą
+                    swoją wiedzę a kurwa od 4 miesięcy nie mogę znaleźć pracy. Przerasta mnie to. Nie wiem
+                    co
+                    mam robić.
+
+                </p>
+            </div>
+            <div class="flex justify-start gap-1 text-gray-400 py-1 text-sm">
+                <div class="hover:bg-[#2d2d2e] rounded-sm p-1 cursor-pointer flex items-center gap-1">
+                    <IconComments/>
+                    188 Comments
+                </div>
+                <div class="hover:bg-[#2d2d2e] rounded-sm p-1 cursor-pointer flex items-center gap-1">
+                    <IconShare/>
+                    Share
+                </div>
+                <div class="hover:bg-[#2d2d2e] rounded-sm p-1 cursor-pointer flex items-center gap-1">
+                    <IconBookmark/>
+                    Save
+                </div>
+                <div class="hover:bg-[#2d2d2e] rounded-sm p-1 cursor-pointer flex items-center">
+                    <IconDots/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import IconComments from "@/components/icons/IconComments.vue";
+import IconShare from "@/components/icons/IconShare.vue";
+import IconBookmark from "@/components/icons/IconBookmark.vue";
+import IconDots from "@/components/icons/IconDots.vue";
+import ArrowUpIcon from "@/components/icons/ArrowUpIcon.vue";
+import ArrowDownIcon from "@/components/icons/ArrowDownIcon.vue";
+
+export default {
+    name: 'CommentsMainContent',
+    components: {ArrowDownIcon, ArrowUpIcon, IconDots, IconBookmark, IconShare, IconComments}
+}
+</script>
---
+++ Frontend/src/views/CommentsTextArea.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <p>Comment as <span class="text-blue-600 hover:underline cursor-pointer">user</span></p>
+        </div>
+        <div class="border border-gray-600 focus-within:border-white rounded-md ">
+                        <textarea
+                            class="text-white pl-3 p-1 rounded-t-md bg-[#1a1a1b] outline-0 w-full min-h-40 resize-y"
+                            placeholder="What are your thoughts?"
+                            oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
+            <div class="bg-[#272729] rounded-b-md flex">
+                <div class="grow" id="edits"></div>
+                <div class="p-2">
+                    <button class="rounded-full bg-white px-2 text-black ">Comment</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'CommentsTextArea'
+}
+</script>
