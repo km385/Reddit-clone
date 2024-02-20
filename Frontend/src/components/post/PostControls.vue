8a6e32bbdf6d4f7ec74c764e849543275b791910
Jakub Furtak <kuba.furtak.pl@gmail.com>
2024-02-20 12:21:05 +0100

    added styles to the post controls

--- Frontend/src/components/post/PostControls.vue
+++ Frontend/src/components/post/PostControls.vue
@@ -3,22 +3,56 @@ import ArrowUpIcon from "@/components/icons/ArrowUpIcon.vue";
 import ArrowDownIcon from "@/components/icons/ArrowDownIcon.vue";
 import IconComments from "@/components/icons/IconComments.vue";
 import IconShare from "@/components/icons/IconShare.vue";
+import {ref} from "vue";
+import ArrowUpFilledIcon from "@/components/icons/ArrowUpFilledIcon.vue";
+import ArrowDownFilledIcon from "@/components/icons/ArrowDownFilledIcon.vue";
 
+const isUpVoted = ref(false)
+const isDownVoted = ref(false)
 
+function upVote() {
+    isUpVoted.value = !isUpVoted.value
+    isDownVoted.value = false
+}
+
+function downVote() {
+    isDownVoted.value = !isDownVoted.value
+    isUpVoted.value = false
+}
 </script>
 <template>
     <div class="flex justify-start gap-1 text-white py-1 text-sm">
-        <div class="bg-[#1a282d] rounded-full cursor-pointer flex ">
-            <div class="hover:text-red-600 hover:bg-hover-dark flex rounded-full p-2">
-                <arrow-up-icon class="w-4 "/>
+        <div @click.stop
+             :class="{'bg-orange-500':isUpVoted, 'bg-blue-500':isDownVoted}"
+             class="bg-[#1a282d] rounded-full flex cursor-default">
+            <div @click="upVote"
+                 :class="{'hover:text-white':isUpVoted || isDownVoted,
+                 'hover:bg-[#d93a00]':isUpVoted,
+                 'hover:bg-[#6a5cff]':isDownVoted}"
+                 class="hover:text-red-600 hover:bg-hover-dark flex rounded-full p-2 cursor-pointer">
+                <div v-if="!isUpVoted">
+                    <arrow-up-icon class="w-4"/>
+                </div>
+                <div v-else>
+                    <ArrowUpFilledIcon/>
+                </div>
             </div>
 
             <div class="flex items-center">
                 2332
             </div>
 
-            <div class="hover:text-blue-600 hover:bg-hover-dark flex rounded-full p-2">
-                <arrow-down-icon class="w-4"/>
+            <div @click="downVote"
+                 :class="{'hover:text-white':isUpVoted || isDownVoted,
+                 'hover:bg-[#d93a00]':isUpVoted,
+                 'hover:bg-[#6a5cff]':isDownVoted}"
+                 class="hover:text-blue-600 hover:bg-hover-dark flex rounded-full p-2 cursor-pointer">
+                <div v-if="!isDownVoted">
+                    <arrow-down-icon class="w-4"/>
+                </div>
+                <div v-else>
+                    <ArrowDownFilledIcon class=""/>
+                </div>
             </div>
         </div>
 
@@ -27,7 +61,7 @@ import IconShare from "@/components/icons/IconShare.vue";
             188
         </div>
 
-        <div class="hover:bg-hover-dark bg-[#1a282d] rounded-full px-4 py-2 cursor-pointer flex items-center gap-1">
+        <div @click.stop class="hover:bg-hover-dark bg-[#1a282d] rounded-full px-4 py-2 cursor-pointer flex items-center gap-1">
             <IconShare/>
             Share
         </div>
