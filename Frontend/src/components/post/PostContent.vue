4dbacc93459922d8c736b1e2b3b0626f89556172
Jakub Furtak <kuba.furtak.pl@gmail.com>
2024-02-28 13:33:43 +0100

    moved the full screen image preview to its own component

--- Frontend/src/components/post/PostContent.vue
+++ Frontend/src/components/post/PostContent.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
 import {ref} from "vue";
-import IconX from "@/components/icons/IconX.vue";
-
-const isImageShown = ref(false)
-function showImage() {
-    isImageShown.value = !isImageShown.value
-    document.body.style.overflow = 'hidden'
-}
-
-function closeImage() {
-    isImageShown.value = false
-    document.body.style.overflow = 'visible'
-}
+import ImageFullScreen from "@/components/post/ImageFullScreen.vue";
 
 const imageSrc = ref("https://preview.redd.it/raniuszek-ponad-po%C5%82owa-zwierz%C4%99cia-to-ogon-ptasi-poniedzia%C5%82ek-v0-ahk5h64q0ijc1.jpeg?width=640&crop=smart&auto=webp&s=2f117f11d02397b3ec3e6b85fc25ae6dc273f0d3")
+const isImageShown = ref(false)
+
 </script>
 <template>
     <div class="py-3" @click.stop >
@@ -33,20 +24,12 @@ const imageSrc = ref("https://preview.redd.it/raniuszek-ponad-po%C5%82owa-zwierz
                 wszystko co mogło pójść źle idzie źle. Nie wiem co mam dalej robić
             </p>
         </div>
-        <div @click="showImage" class="border border-gray-600 rounded-xl flex justify-center" v-else-if="true">
+        <div @click="isImageShown = true" class="border border-gray-600 rounded-xl flex justify-center" v-else-if="true">
             <img class="object-contain max-h-[550px]"
                  :src="imageSrc"
                  alt="image">
             <transition name="slide-fade">
-                <div
-                    :style="{ 'background-image': `url(${imageSrc})`}"
-                    @click.stop v-if="isImageShown" class="fixed inset-0 bg-gray-400 z-[9999] cursor-default">
-                    <icon-x @click="closeImage"
-                            class="z-10 fixed top-2 right-2 p-2 bg-black text-white rounded-full w-10 h-10 cursor-pointer"/>
-                    <img class="object-contain h-full w-full backdrop-blur-3xl"
-                         :src="imageSrc"
-                         alt="image">
-                </div>
+                <ImageFullScreen v-if="isImageShown" @close="isImageShown = false" :url="imageSrc"/>
             </transition>
         </div>
         <div v-else>
@@ -60,7 +43,7 @@ const imageSrc = ref("https://preview.redd.it/raniuszek-ponad-po%C5%82owa-zwierz
 <style scoped>
 .slide-fade-enter-active,
 .slide-fade-leave-active {
-    transition: all 0.1s;
+    transition: all 0.3s;
 }
 .slide-fade-enter-from,
 .slide-fade-leave-to
